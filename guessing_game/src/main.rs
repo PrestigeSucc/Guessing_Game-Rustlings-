b89f752c243ddf5ba95f0cc76867dd9151b4dd3a
PrestigeSucc <jobsterq2006@gmail.com>
2026-06-15 01:26:22 -0500

    Remove comments
    I decided to remove the comments to further test my memory on what each
    line does/means, I will be adding the comments to other things as I go
    throughn the rust book!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,11 @@
-use std::io; /* To obtain user input and the print the result as output, we need to bring the 'io' input/output library into scope.
-The io library comes from the standard library, known as 'std': */
-// If a type you want to use isn't in the prelude, you need to bring it into scope via a 'use'
-// statement
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess."); /* 'println!' is a macro that prints a string to the screen */
+    println!("Please input your guess.");
 
-    let mut guess = String::new(); /* We use the 'let' statement to create the variable and 'mut' to make it mutable;
-    On the right of the '=' sign is the value that guess is bound to, which is the result if calling 'String::new', a funtion that
-    returns a new instance of 'String'. 'String' is a string type provided by the standard library that is a growable UTF-8
-    encoded bit of text. The '::' syntax in the '::new' line indicates that 'new' is an associated function of the 'String' type. An
-    associated function is a function thats implemented on a type, ie 'String'. This 'new' function creates a new, empty string.
-    In full, the 'let mut guess = String::new();' line has created a mutable variable that is currently bound to a new, empty instance
-    of a 'String'.
-     */
+    let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
@@ -23,6 +13,3 @@ fn main() {
 
     println!("You guessed: {guess}");
 }
-// The 'main' function is the entry point into the program
-// The 'fn' syntax declares a new function; the parentheses, '()', indicate there are no
-// parammeters; and the curly bracket, '{', starts the body of a function
